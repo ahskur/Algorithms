c08befa463931534885f83a9906f0c95cf78e0b7
ahskur <63268217+ahskur@users.noreply.github.com>
2023-10-22 13:31:23 +0200

    Update HappyNumbers.java

--- HappyNumbers.java
+++ HappyNumbers.java
@@ -3,27 +3,41 @@
 
 public class HappyNumbers {
     public static boolean isHappy(int n) {
-        // This method checks if a number is a happy number.
-        // A happy number is defined as follows: Start with any positive integer,
-        // replace the number by the sum of the squares of its digits, and repeat
-        // the process until the number equals 1, or it loops endlessly in a cycle
-        // that does not include 1. If it equals 1, it's a happy number; otherwise, it's not.
-
+        // Initialisiert alle variablen, bzw summe, die am Anfang immer gleich 0 sein soll
+        // und die Anzahl an Zyklen, der das programm laufen soll.
+        // Falls die Anzahl überschritten wird, sofort abbrechen, weil der Wert nie fröhlich sein wird
         int iterations = 0;
         int sum = 0;
+        // Schleife läuft, solange der angegebene Wert 1 ist, bis es eine Lösung (fröhlich oder nicht) gefunden würde
         while (n > 0) {
+            System.out.println(n);
+            // Jedes Mal, das eine Ziffer getestet wird, 1 an die Anzahl an Zyklen addieren
             iterations++;
-            int ziffer = n % 10;  // Get the last digit of n.
+            // Hole letzten Ziffer von n (immer von rechts nach links z.B: 82 → Erst 2 wird angerechnet, dann 8)
+            int ziffer = n % 10;
             System.out.println("Ziffer: " + ziffer);
-            int quader = ziffer * ziffer;  // Square the digit.
+            // Ziffer wird quadriert und summiert
+            int quader = ziffer * ziffer;
             System.out.println("Quadriert: " + quader);
-            sum = sum + quader;  // Add the squared digit to the sum.
+            sum = sum + quader;
             System.out.println("Summe: " + sum);
-            if (sum == 1 && n / 1 == 1) return true;  // Check if it's a happy number.
-            // For the number to be happy, the sum needs to be 1 but the last digit itself also needs to be 1
-            // This catches cases where the number return sum 1 by still has values
-            n = n / 10;  // Remove the last digit from n.
-            if (iterations > 15) return false;  // Avoid infinite loops.
+            // Falls die Summe gleich 1 ist, abbrechen und return true → Zahl ist fröhlich.
+            // Dabei muss aber aufgepasst werden: Falls die erste anzurechnen Ziffer '1' ist,
+            // dann ist die Summe sofort eins und wird falschweise abgebrochen.
+            // z.B: 81 → Ziffer '1' wird zuerst gelesen, quadriert und an 'var' sum addiert, d.h summe == 1, daher abbrechen.
+            // Um das zu vermeiden, wird versucht die ganze Zahl 'n' durch '1' zu teilen. Falls es != 1 ist,
+            // bedeutet, dass die Zahl nicht vollständig gelesen würde bzw. nicht alle Ziffer gefunden worden sind also nicht abbrechen.
+            if (sum == 1 && n/1 == 1) return true;
+            // Letzte Ziffer von n fällt weg, weil es schon quadriert und summiert würde
+            n = n / 10;
+            // Prüft, ob die Anzahl an maximale Zyklen erreicht würde,
+            // wenn ja, abbrechen, weil die Summe sich unendlich wiederholt
+            if (iterations > 13) return false;
+            // Falls n = 0 ist, das bedeutet, dass alle Ziffern gefunden und summiert worden sind.
+            // Setze dann n als die Werte der Summe, sodass die Summe getestet sein kann, bis Summe = 1 erreicht würde.
+            // z.B: 19 → 82 → 68 → 100 → 1
+            // Summe muss auch dabei wieder = 0 gesetzt werden,
+            // sonst addieren sich die Ziffern der letzte Vorgang auch dazu
             if (n == 0) {
                 n = sum;
                 sum = 0;
@@ -33,9 +47,11 @@ public class HappyNumbers {
     }
 
     public static int nextHappyNumber(int n) {
-        // This method finds the next happy number greater than the given number n.
+        // Während Schleife läuft, immer +1 an 'n' addieren, sodass immer die nächste Zahl versucht wird
         while (true) {
             n++;
+            // Dann neues 'n' testen, falls es fröhlich ist, wird die Schleife am main abgebrochen
+            // und geprinted
             if (isHappy(n)) {
                 return n;
             }
@@ -43,14 +59,16 @@ public class HappyNumbers {
     }
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 103;
         if (isHappy(n)) {
-            System.out.println(n + " ist froehlich.");
+            System.out.println(n + " ist fröhlich.");
         } else {
             System.out.println(n + " ist traurig.");
+            // Falls Zahl ist traurig, versuche die nächste Zahl zu finden, die fröhlich ist
             int next = nextHappyNumber(n);
-            System.out.println("Die nächste froehliche Zahl nach " + n + " ist " + next);
+            System.out.println("Die nächste fröhliche Zahl nach " + n + " ist " + next);
+        }
         }
     }
-}
+
 
